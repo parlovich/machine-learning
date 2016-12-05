8af618d1bba3e3c2b1aab0bea20ee69234fc4475
Pavel Arlovich <pavelarlovich@elance-odesk.com>
2016-12-05 22:37:47 +0300

    prediction

--- ex2/predict.m
+++ ex2/predict.m
@@ -14,12 +14,8 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
-
+pos = find(sigmoid(X * theta) > 0.5);
+p(pos, 1) = 1;
 
 % =========================================================================
 
